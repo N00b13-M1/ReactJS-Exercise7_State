a92d5d887ae017e9cce785556e36c32727644c3f
N00b13-M1 <michael.cornelis@gmail.com>
2022-02-09 01:24:32 +0100

    StateExPhase2

--- src/App.js
+++ src/App.js
@@ -1,39 +1,72 @@
-import React, { Component } from 'react';
-import Perso from './components/Perso';
+import React, { Component } from 'react'
+import Perso from './components/Perso'
 
 class App extends Component {
-
+  // constructor(props) {
+  //   super(props);
+  //     this.state = 
+  // }
+  
   state = [
     {
-      prenom: "Elias",
-      nom: "Ling",
-      age: 23
+      prenom: "Plop",
+      nom: "X",
+      age: 14,
     },
     {
-      prenom: "Tom",
-      nom: "Ping",
-      age: 12
+      prenom: "Sammy",
+      nom: "Y",
+      age: 15,
+
     },
     {
-      prenom: "Hannah",
-      nom: "K",
-      age: 19
+      prenom: "Nico",
+      nom: "Z",
+      age: 20,
+
     }
   ]
-  render() {
 
+  onShowLogParent = () =>{
+    this.setState([
+      {
+        prenom: "Elias",
+        nom: "X",
+        age: 25,
+      },
+      {
+        prenom: "Sammy",
+        nom: "Y",
+        age: 15,
+  
+      },
+      {
+        prenom: "Nico",
+        nom: "Z",
+        age: 20,
+  
+      }
+    ])
+  }
+
+  render() {
     console.log(this.state)
     return (
-      <>
-        {
-          this.state.map(element =>
-            <p>Salut les amis je m'apelle {element.prenom} {element.nom} et j'ai {element.age}</p>
-            )
-        }
-        {/* <Perso/> */}
-      </>
-    );
+
+      <div>
+        {/* {this.state.map(element =>
+          <h1>Hello c'est ici {element.prenom} {element.nom}! J'ai {element.age} ans </h1>)
+        } */}
+        <Perso 
+          prenom={this.state[0].prenom} 
+          nom={this.state[0].nom} 
+          age={this.state[0].age}
+          onShowLog={this.onShowLogParent}
+        />
+      </div>
+
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Perso.js
+++ src/components/Perso.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+// let prenom = "pio"
+
 class Perso extends Component {
+    showLog = () =>{
+        // console.log("plop")
+        this.props.onShowLog()
+    }
     render() {
         return (
             <div>
-                Plop
+                {/* <p>{this.props.nom}</p>
+                <p>{this.nom}</p>
+                <p>{prenom}</p> */}
+                <p>Hello je m'apelle {this.props.prenom} {this.props.nom} et j'ai {this.props.age} ans </p>
+                <button onClick={()=>this.showLog()}>Button</button>
             </div>
         );
     }
